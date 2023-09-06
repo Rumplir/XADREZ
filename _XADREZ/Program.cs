56b6339e9a3a15e5fd0e1fd5eca58850bc5c73a2
Matheus Rocha <Rumplir@gmail.com>
2023-09-06 17:38:13 -0300

    Update no metodo imprimirTabuleiro

--- _XADREZ/Program.cs
+++ _XADREZ/Program.cs
@@ -18,6 +18,8 @@ namespace _XADREZ
                 tab.colocarPeca(new Rei(Cor.Black, tab), new Posicao(0, 0));
                 tab.colocarPeca(new Rei(Cor.Black, tab), new Posicao(5, 4));
                 tab.colocarPeca(new Rei(Cor.Black, tab), new Posicao(7, 4));
+                tab.colocarPeca(new Torre(Cor.White, tab), new Posicao(6, 2));
+                tab.colocarPeca(new Torre(Cor.White, tab), new Posicao(5, 6));
                 Tela.ImprimirTabuleiro(tab);
             }
             catch (Exception ex)
--- _XADREZ/Tela.cs
+++ _XADREZ/Tela.cs
@@ -13,6 +13,7 @@ namespace _XADREZ
         {
             for (int i = 0; i < tab.Linhas;  i++)
             {
+                Console.Write($"{8-i} ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i,j) == null)
@@ -21,10 +22,31 @@ namespace _XADREZ
                     }
                     else
                     {
-                        Console.Write($"{tab.peca(i,j)} ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write($" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.White)
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
