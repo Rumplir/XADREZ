5741c3a43a70c027cf733d0df13d38fe3133467c
Matheus Rocha <Rumplir@gmail.com>
2023-09-06 15:37:40 -0300

    Criação e inicio da implementação do tabuleiro

--- _XADREZ/Program.cs
+++ _XADREZ/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace _XADREZ
+﻿using _XADREZ.tabuleiro;
+
+namespace _XADREZ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.WriteLine("a");
         }
     }
 }
---
+++ _XADREZ/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _XADREZ.tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QntMovimentos { get; set; }
+        public Tabuleiro tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            this.tabuleiro = tabuleiro;
+            QntMovimentos = 0;
+        }
+    }
+}
